67a09fc95f1ce9fa5d98fa13931487bef52c280e
Josep Valur Guðlaugsson <josep.gudlaugsson@origo.is>
2024-03-05 03:56:59 +0000

    Added stuff for icewallet nodejs client demo

---
+++ IsveskiDemo_NodeJS/README.md
@@ -0,0 +1,3 @@
+# IsveskiDemo_NodeJS
+
+
---
+++ IsveskiDemo_NodeJS/servercode/api/apis.ts
@@ -0,0 +1,14 @@
+export * from './serverEndpointApi';
+import { ServerEndpointApi } from './serverEndpointApi';
+import * as http from 'http';
+
+export class HttpError extends Error {
+    constructor (public response: http.IncomingMessage, public body: any, public statusCode?: number) {
+        super('HTTP request failed');
+        this.name = 'HttpError';
+    }
+}
+
+export { RequestFile } from '../model/models';
+
+export const APIS = [ServerEndpointApi];
